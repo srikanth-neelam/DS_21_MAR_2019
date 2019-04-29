85649724ebe4839046fbc533ddea4011c3b3f828
srikanthpragada <srikanthpragada@gmail.com>
2019-04-29 07:59:42 +0530

    29-APR

--- web/loan/views.py
+++ web/loan/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render
-from django.http import JsonResponse
 import pickle
 
+from django.http import JsonResponse
+from django.shortcuts import render
+
+
 def loan_client(request):
-    return render(request,'loan_client.html')
+    return render(request, 'loan_client.html')
+
 
 def loan_status_check(request):
     # read parameters
@@ -11,15 +14,14 @@ def loan_status_check(request):
     income = float(request.GET["income"])
     cincome = float(request.GET["cincome"])
     term = float(request.GET["term"])
+    cr = float(request.GET["cr"])
 
     # load model and make prediction
-    f = open(r"e:\classroom\ds\jan21\loan_model.pkl","rb")
+    f = open(r"e:\classroom\ds\mar21\loan\lr_model.pkl", "rb")
     model = pickle.load(f)
     f.close()
-
-    X_test = [[income,cincome, amount, term]]
+    X_test = [[amount,income, cincome,term,cr]]
+    print(X_test)
     status = model.predict(X_test)[0]
 
-    return JsonResponse({"status" : status})
-
-
+    return JsonResponse({"status": status})
